a438ef8df84aed9f6192ac10c2084f4b7937c6e7
Aman Harwara <amanharwara@protonmail.com>
2022-02-15 00:25:49 +0530

    feat: add workbox webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const mergeWithEnvDefaults = require('./webpack-defaults');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 require('dotenv').config();
 
 module.exports = (env) => {
@@ -21,6 +22,11 @@ module.exports = (env) => {
         // Options similar to the same options in webpackOptions.output
         filename: './stylesheets/app.css',
         ignoreOrder: true, // Enable to remove warnings about conflicting order
+      }),
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 5000000,
       }),
     ],
     resolve: {
